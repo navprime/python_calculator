e80d9e750e69659f0cfd008d77aaae75ccadc49d
navprime <navneetpanda003@gmail.com>
2020-08-08 16:46:18 +0530

    calculator

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 root.title("Calculator")
 root.geometry("220x300")
 
-frame1 = tk.frame(root, bgcolor="black")
+frame1 = tk.Frame(root, bg="#000000")
 frame1.pack()
 
 root.mainloop()
